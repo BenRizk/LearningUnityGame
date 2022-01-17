d642df6aa353cd5cd20e61e9ff2546ed44bc0d65
BenRizk <coolguyben@hotmail.com>
2022-01-17 14:48:36 -0500

    New Sprites And Despawning
    Added sprites for the scene and added script that lets game objects remove themselves.

---
+++ Unity2019 2D-test/Assets/test scripts/Despawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawner : MonoBehaviour
+{
+    public int timer = 0;
+    public int killTime = 500;
+    public string myClone;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > killTime)
+        {
+            timer = 0;
+            if(this != null)
+            {
+                if (this.gameObject.name.Equals(myClone))
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+        else if(this.gameObject.name.Equals(myClone))
+        {
+            timer++;
+        }
+    }
+}
